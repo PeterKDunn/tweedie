4ec019444f82e72d1bbfa5bb05d7deaa7750a4f0
Peter Dunn <pdunn2@usc.edu.au>
2025-10-01 17:20:35 +1000

    Reduce error tolerance just a bot

--- src/findKmax.f
+++ src/findKmax.f
@@ -9,8 +9,8 @@
       EXTERNAL findImkZero, myfloor
       COMMON /params/ Cp, Cy, Cmu, Cphi, pSmall
       
-      aimrerr = 1.0d-10
-      pi = 4.0d0 * DATAN(1.0d0)
+      aimrerr = 1.0d-09
+      pi = 4.0d00 * DATAN(1.0d00)
 
       IF ( pSmall) THEN
         IF (Cy .GE. Cmu) THEN
@@ -41,9 +41,10 @@
         ELSE
 *         Cy < Cmu and p > 2
           tmax = rtnewton(findImkZero, 
-     &                    startPoint * 0.15, 
+     &                    startPoint * 0.15d00, 
      &                    startPoint * 30.0d00, 
-     &                    startPoint, aimrerr)
+     &                    startPoint, 
+     &                    aimrerr)
 *         funcd returns the fn value, and derivative value
 
 *         Find kmax, mmax
