adf4ade78ca6605e156f547cc02d50c76fa024e0
Peter Dunn <pdunn2@usc.edu.au>
2025-10-03 09:44:10 +1000

    remove m COMMON block mparam

--- src/findExactZeros.f
+++ src/findExactZeros.f
@@ -2,20 +2,17 @@
 *     Finds the exact zeros, to integrate between
 
       IMPLICIT NONE
-      INTEGER m
       DOUBLE PRECISION zeroR, zeroL, zero, zeroSP
       DOUBLE PRECISION Cp, Cy, Cmu, Cphi
       DOUBLE PRECISION xacc, rtnewton
       LOGICAL pSmall
       COMMON /params/ Cp, Cy, Cmu, Cphi, pSmall
-      COMMON /mparam/ m
       EXTERNAL findImkM
       
       xacc = 1.0d-09
 *     Sometimes, the curve is quite flat and accuracy is hard to 
 *     achieve, but not vital anyway
-*      write(*,*) "findExactZeros: ", zeroSP
-*      write(*,*) "              : ", zeroL, zeroR
+
       zero = rtnewton(findImkM, zeroL, zeroR, zeroSP, xacc)
 
       RETURN
