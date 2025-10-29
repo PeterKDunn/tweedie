db27c64ca637057a3034ac4f44b474de63e47a92
Peter Dunn <pdunn2@usc.edu.au>
2025-10-30 09:46:44 +1000

    Minor edit

--- R/ptweedie_Inversion.R
+++ R/ptweedie_Inversion.R
@@ -14,7 +14,8 @@ ptweedie.inversion <- function(q, mu, phi,  power, verbose = FALSE, details = FA
   ### BEGIN SET UP
   N <- as.integer( length(q) )
   cdf <- as.double(rep(0, N))
-  pSmall  <- ifelse( (power > 1) & (power < 2), TRUE, FALSE )
+  pSmall  <- ifelse( (power > 1) & (power < 2),
+                     TRUE, FALSE )
 
   # Initialise
   exitstatus_scalar <- as.integer(0)
