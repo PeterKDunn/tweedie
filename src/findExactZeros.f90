9e696b079f1b12dff77f666e50b78a64606dec21
Peter Dunn <pdunn2@usc.edu.au>
2025-10-31 16:36:24 +1000

    Add verbose comments

--- src/findExactZeros.f90
+++ src/findExactZeros.f90
@@ -84,6 +84,8 @@ SUBROUTINE findExactZeros(i, m, tL, tR, tStart, tZero, leftOfMax)
   IF ( (fl * fR) .GT. 0.0_C_DOUBLE ) THEN
     ! Then bounds do not actually bound the zero
     IF (Cverbose) WRITE(*,*) "Bounds do not bracket the zero."
+    IF (Cverbose) WRITE(*,*) "- Bounds:", tL, tR
+    IF (Cverbose) WRITE(*,*) "- Values:", fL, fR
   END IF
 
   IF ( (Cpsmall) .AND. (current_y .LT. current_mu) ) THEN
