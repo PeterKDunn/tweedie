34a7b7bcec6c86387debcead47534dfb7178b1b9
Peter Dunn <pdunn2@usc.edu.au>
2025-11-06 17:54:12 +1000

    Fix some testing

--- tests/testthat/test-edgeValues.R
+++ tests/testthat/test-edgeValues.R
@@ -1,21 +1,25 @@
-test_that("Zero input returns zero (ignoring attributes and noise)", {
-  
-  # The test environment is adding a dimension attribute and seeing numerical noise.
+test_that("Zero first inputs returns correct values", {
   
   # 1. Test dtweedie(0, ...)
   expect_equal(
-    dtweedie(0, mu = 1, phi = 1, power = 3.5), 
-    0,
-    tolerance = 1e-8,      # Required to pass the numerical check (actual != 0)
-    ignore_attr = TRUE     # Required to pass the dimension check (dim(actual) != dim(expected))
+    dtweedie(0, mu = 1, phi = 1, power = 1.5), 
+    exp( -1^(2-1.5)/((1 * (2 - 1.5))))
   )
   
+  expect_equal(
+    dtweedie(0, mu = 1, phi = 1, power = 3.5), 
+    0
+  )
+
   # 2. Test ptweedie(0, ...)
   expect_equal(
+    ptweedie(0, mu = 1, phi = 1, power = 1.5), 
+    exp( -1^(2-1.5)/((1 * (2 - 1.5))))
+  )
+
+  expect_equal(
     ptweedie(0, mu = 1, phi = 1, power = 3.5), 
-    0,
-    tolerance = 1e-8,
-    ignore_attr = TRUE
+    0
   )
 })
 
