1cb3f08c6d20ef20e22691acc8417bd74637ade4
Peter Dunn <pdunn2@usc.edu.au>
2025-11-24 08:59:45 +1000

    Add more deprecation tests

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -1,9 +1,15 @@
 
 test_that("Deprecated functions work", {
-  expect_no_error(
-    ptweedie.inversion(0.001, mu = 0.01, phi = 0.01, power = 6, verbose=FALSE)  
+  expect_warning(
+    ptweedie.inversion(1, mu = 1, phi = 1, power = 6, verbose=FALSE)  
   )
-  expect_no_error(
-    dtweedie.inversion(0.001, mu = 0.01, phi = 0.01, power = 6, verbose=FALSE)  
+  expect_warning(
+    dtweedie.inversion(1, mu = 1, phi = 1, power = 6, verbose=FALSE)  
+  )
+  expect_warning(
+    dtweedie.series(1, mu = 1, phi = 1, power = 6)  
+  )
+  expect_warning(
+    ptweedie.series(1, mu = 1, phi = 1, power = 1.5)  
   )
 })  
