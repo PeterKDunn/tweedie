8c8974e6aecb40978282a330216734b4f010e56a
Peter Dunn <pdunn2@usc.edu.au>
2025-11-23 14:23:17 +1000

    Rename, add some tests

--- tests/testthat/test-other-ptweedie.R
+++ tests/testthat/test-other-ptweedie.R
@@ -13,5 +13,13 @@ test_that("No common errors", {
     ptweedie_inversion(0.001, mu = 5, phi = 0.01, power = 6, verbose=FALSE)  )
   expect_no_error(
     ptweedie_inversion(0.075075, mu = 1, phi = 1, power = 4.5, verbose=FALSE)  )
+  expect_no_error(
+    ptweedie_inversion(.001, power=6, mu=.01, phi=.01)  )
+  expect_no_error(
+   ptweedie_inversion(0.001, phi = 10, p = 1.01, mu = 5)  )
+  expect_no_error(
+    ptweedie_inversion(0.001, mu = 1, phi = 0.01, p = 1.01)  )
+  expect_no_error(
+  ptweedie_inversion(50, mu=1.4, phi=0.74, power=3) )
 })
 
