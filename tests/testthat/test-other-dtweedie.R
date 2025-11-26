30118d50d9a1e5dce4a43cba66531455723d9444
Peter Dunn <pdunn2@usc.edu.au>
2025-11-26 15:34:29 +1000

    One quick test

--- tests/testthat/test-other-dtweedie.R
+++ tests/testthat/test-other-dtweedie.R
@@ -4,5 +4,8 @@ test_that("No common errors", {
   expect_no_error(
     dtweedie_inversion(power=1.5, mu=1, phi=0.5513086, y=0.5557088)
   )
+  expect_no_error(
+    dtweedie(power=1.5, mu=1, phi=0.5513086, y=c(0, 1, 2) )
+  )
 })
 
