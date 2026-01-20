364eb83d88edda41bfd6c90de618db66ab646ac9
Peter Dunn <pdunn2@usc.edu.au>
2026-01-20 15:03:44 +1000

    Update hex

--- man/figures/make_tweedie_hex.R
+++ man/figures/make_tweedie_hex.R
@@ -1,3 +1,5 @@
+options(bitmapType = "cairo")
+
 library(hexSticker)
 library(ggplot2)
 library(tweedie)
@@ -6,7 +8,7 @@ library(tweedie)
 
 set.seed(1)
 
-x <- seq(0, 4, length.out = 400)
+x <- seq(0.001, 4, length.out = 400)
 
 ## Tweedie parameters (chosen for visual shape, not pedagogy)
 mu   <- 1.2
@@ -23,20 +25,28 @@ df <- data.frame(x = x, y = y)
 density_plot <- ggplot(df, aes(x, y)) +
   geom_line(linewidth = 1.2, 
             colour = "#2C3E50") +
-  theme_void() +
-  coord_cartesian(expand = FALSE)
+  coord_cartesian(
+    xlim = c(0, max(x)),
+    expand = FALSE
+  ) +
+  theme_void()
+
 
 ## ---- Create hex sticker ----
+options(bitmapType = "cairo")
 
 sticker(
   subplot    = density_plot,
   package    = "tweedie",
-  p_size     = 18,
+  p_size     = 20,
   p_color    = "#2C3E50",
   s_x        = 1,
   s_y        = 0.8,
-  s_width    = 0.75,
+  s_width    = 0.7,
   h_fill     = "white",
   h_color    = "#2C3E50",
-  filename   = "tweedie_hex.png"
+  h_size     = 2,
+  #white_around = 0.08,
+  white_around_sticker = TRUE,
+  filename   = "tweedie_hex.pdf"
 )
